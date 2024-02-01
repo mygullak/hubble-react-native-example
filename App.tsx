3e42dcf698e01d3030fe94377de651345f67a2e2
junaid <junaid1460@gmail.com>
2024-02-01 22:33:44 +0530

    feat: webview example

--- App.tsx
+++ App.tsx
@@ -5,58 +5,79 @@
  * @format
  */
 
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import {
-  StyleSheet
+  BackHandler,
+  SafeAreaView
 } from 'react-native';
 import WebView from 'react-native-webview';
 
+type HubbleENV = 'debug' | 'prod';
+type HubbleParams = {
+  authToken: string;
+  appVersion: string;
+  appBuildNumber: string;
+  appPackageName: string;
+  clientId: string;
+  clientSecret: string;
+  env: HubbleENV;
+}
 
 
 const HubbleWebview = () => {
+
+  const paramsStr = JSON.stringify(
+    {
+      authToken: 'something',
+      appVersion: 'appVersion',
+      appBuildNumber: 'appBuildNumber',
+      appPackageName: 'appPackageName',
+      clientId: 'something',
+      clientSecret: 'something',
+      env: 'debug'
+    } as HubbleParams
+  );
   const injectedJavaScriptBeforeContentLoaded = `
-      window.hubbleParams = function() {
-        return  JSON.stringify(
-          {  
-            authToken: '4444444444',
-            appVersion: 'appVersion',
-            appBuildNumber: 'appBuildNumber',
-            appPackageName: 'appPackageName',
-            clientId: 'niyo',
-            clientSecret: 'clientSecret',
-            env: "prod"
-          }
-        );
-      }
+    window.hubbleParams = function() {
+      return '${paramsStr}';
+    };
   `;
 
+  const webViewRef = useRef<WebView>(null);
+
+  const handleBackButtonPress = () => {
+    try {
+      webViewRef.current?.injectJavaScript("window.hubbleBackButtonPressed()")
+    } catch (err) {
+      console.log("[handleBackButtonPress] Error : ", err)
+    }
+
+    return true
+  }
+
+  useEffect(() => {
+    BackHandler.addEventListener("hardwareBackPress", handleBackButtonPress)
+    return () => {
+      BackHandler.removeEventListener("hardwareBackPress", handleBackButtonPress)
+    };
+  }, []);
+
+
   // Add safe areas add needed
-  return <WebView
-    webviewDebuggingEnabled={true} // Disable at your end
-    source={{ uri: 'https://gullak-18d3b.web.app/#/' }}
-    injectedJavaScriptBeforeContentLoaded={injectedJavaScriptBeforeContentLoaded}
-    style={{ flex: 1 }}
-  />;
+  return <SafeAreaView style={{ flex: 1 }}>
+    <WebView
+      ref={webViewRef}
+      webviewDebuggingEnabled={true} // Disable at your end
+      cacheEnabled={false} // Disable at your end
+      source={{ uri: 'https://store.myhubble.money' }}
+      injectedJavaScriptBeforeContentLoaded={injectedJavaScriptBeforeContentLoaded}
+      setSupportMultipleWindows={true}
+      javaScriptCanOpenWindowsAutomatically={true}
+    />
+  </SafeAreaView>
 }
 function App(): React.JSX.Element {
-
   return <HubbleWebview />
-
 }
-
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  box: {
-    width: 60,
-    height: 60,
-    marginVertical: 20,
-  },
-});
-
 
 export default App;
